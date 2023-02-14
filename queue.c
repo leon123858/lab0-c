bcdc7a385bd0b62a4c55c209783f51578228aa4e
leon123858 <a0970785699@gmail.com>
2023-02-14 18:57:36 +0800

    basic op OK

--- queue.c
+++ queue.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "list.h"
 #include "queue.h"
 
 /* Notice: sometimes, Cppcheck would find the potential NULL pointer bugs,
@@ -10,38 +11,102 @@
  *   cppcheck-suppress nullPointer
  */
 
+typedef struct list_head list_head;
 
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *lh = malloc(sizeof(list_head));
+    if (lh != NULL) {
+        lh->next = lh;
+        lh->prev = lh;
+    }
+    return lh;
 }
 
 /* Free all storage used by queue */
-void q_free(struct list_head *l) {}
+void q_free(struct list_head *l)
+{
+    if (l == NULL) {
+        return;
+    }
+    list_head *next = l->next;
+    while (next != l) {
+        list_head *tmp = next;
+        next = next->next;
+        element_t *element = container_of(tmp, element_t, list);
+        free(element->value);
+        free(element);
+    }
+    free(l);
+}
 
 /* Insert an element at head of queue */
 bool q_insert_head(struct list_head *head, char *s)
 {
+    if (head == NULL)
+        return false;
+    size_t length = strlen(s) + 1;  // 因 strcpy 會多抓一個結尾字元
+    char *str = malloc(length);
+    element_t *element = malloc(sizeof(element_t));
+    if (str == NULL || element == NULL)
+        return false;
+    str = strncpy(str, s, length);
+    element->value = str;
+    element->list.prev = head;
+    element->list.next = head->next;
+    head->next->prev = &element->list;
+    head->next = &element->list;
     return true;
 }
 
 /* Insert an element at tail of queue */
 bool q_insert_tail(struct list_head *head, char *s)
 {
+    if (head == NULL)
+        return false;
+    size_t length = strlen(s) + 1;  // 因 strcpy 會多抓一個結尾字元
+    char *str = malloc(length);
+    element_t *element = malloc(sizeof(element_t));
+    if (str == NULL || element == NULL)
+        return false;
+    str = strncpy(str, s, length);
+    element->value = str;
+    element->list.prev = head->prev;
+    element->list.next = head;
+    head->prev->next = &element->list;
+    head->prev = &element->list;
     return true;
 }
 
 /* Remove an element from head of queue */
 element_t *q_remove_head(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL || head->next == head)
+        return NULL;
+    element_t *target = container_of(head->next, element_t, list);
+    target->list.next->prev = head;
+    head->next = target->list.next;
+    if (sp != NULL) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return target;
 }
 
 /* Remove an element from tail of queue */
 element_t *q_remove_tail(struct list_head *head, char *sp, size_t bufsize)
 {
-    return NULL;
+    if (head == NULL || head->next == head)
+        return NULL;
+    element_t *target = container_of(head->prev, element_t, list);
+    target->list.prev->next = head;
+    head->prev = target->list.prev;
+    if (sp != NULL) {
+        strncpy(sp, target->value, bufsize - 1);
+        sp[bufsize - 1] = '\0';
+    }
+    return target;
 }
 
 /* Return number of elements in queue */
